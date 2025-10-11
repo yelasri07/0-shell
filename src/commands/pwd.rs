d2ab72f4dde744836177e973968de886a3bde30a
Mossab Idbenkellal <idmossab@gmail.com>
2025-10-11 09:11:57 +0100

    pwd

--- src/commands/pwd.rs
+++ src/commands/pwd.rs
@@ -1,3 +1,8 @@
-pub fn pwd_handler(args: Vec<String>) {
-    println!("{:?}", args);
+use std::env;
+
+pub fn pwd_handler(_args: Vec<String>) {
+    match env::current_dir() {
+        Ok(path) => println!("{}", path.display()),
+        Err(e) => eprintln!("Erreur lors de la récupération du répertoire courant : {}", e),
+    }
 }
