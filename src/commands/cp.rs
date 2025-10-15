e61702b432a4dd42e2285c2ac30c21a61659e3c5
ismailsayen <ismailsvn02@gmail.com>
2025-10-15 14:40:55 +0100

    correction logique cp

--- src/commands/cp.rs
+++ src/commands/cp.rs
@@ -1,51 +1,43 @@
-use std::io::{self, Write};
-use std::path::{Path, PathBuf};
 use std::{
     fs::{self, File},
+    io::{self, Write},
+    path::{Path, PathBuf},
 };
 
 #[derive(Debug, Clone)]
 pub struct Cp {
-    pub dir_flag: bool,
-    pub cible: String,
-    pub destinations: Vec<String>,
+    pub options: Vec<String>,
+    pub target: String,
 }
 
 impl Cp {
     pub fn new() -> Self {
         Self {
-            dir_flag: false,
-            cible: String::new(),
-            destinations: vec![],
+            options: vec![],
+            target: String::new(),
         }
     }
 
-    pub fn read_file(&self) -> io::Result<String> {
-        fs::read_to_string(&self.cible)
-    }
-
     pub fn exec(&self, content: String) -> io::Result<()> {
-        for dest in &self.destinations {
-            match get_file_state(dest) {
-                Ok(meta) => {
-                    if meta.is_file() {
-                        let mut file = File::create(dest)?;
-                        file.write_all(content.as_bytes())?;
-                    } else if meta.is_dir() {
-                        let file_name = Path::new(&self.cible)
-                            .file_name()
-                            .map(|n| n.to_string_lossy().to_string())
-                            .unwrap_or_else(|| "unknown".to_string());
-                        let new_path = Path::new(dest).join(file_name);
-                        let mut file = File::create(&new_path)?;
-                        file.write_all(content.as_bytes())?;
-                    }
-                }
-                Err(_) => {
-                    // destination doesn’t exist → try to create it as a file
-                    let mut file = File::create(dest)?;
+        let src = self.options[0].clone();
+        match get_file_state(&src) {
+            Ok(meta) => {
+                if meta.is_file() {
+                    let mut file = File::create(&self.target)?;
+                    file.write(content.as_bytes())?;
+                } else if meta.is_dir() {
+                    let file_name = Path::new(&src)
+                        .file_name()
+                        .map(|n| n.to_string_lossy().to_string())
+                        .unwrap_or_else(|| "unknown".to_string());
+                    let new_path = Path::new(&self.target).join(file_name);
+                    let mut file = File::create(&new_path)?;
                     file.write_all(content.as_bytes())?;
                 }
+            }
+            Err(_) => {
+                let mut file = File::create(&src)?;
+                file.write_all(content.as_bytes())?;
             }
         }
         Ok(())
@@ -69,7 +61,6 @@ impl Cp {
                 let mut output = File::create(&dest_path)?;
                 io::copy(&mut input, &mut output)?;
             } else if meta.is_dir() {
-                
                 Self::copy_dir_recursive(&child, &dest_path)?;
             }
         }
@@ -84,60 +75,46 @@ pub fn cp_handler(args: Vec<String>) {
     }
 
     let mut cp = Cp::new();
+    cp.target = args[args.len() - 1].clone();
+    for i in 0..args.len() - 1 {
+        cp.options.push(args[i].clone());
+    }
 
-    for file in args {
-        if file == "-r" && !cp.dir_flag {
-            cp.dir_flag = true;
-            continue;
-        } else if file.starts_with('-') && file != "-r" {
-            eprintln!("cp: invalid option '{}'", file);
+    let metadata = get_file_state(&cp.target);
+    if metadata.is_err() {
+        eprintln!("cp: cannot stat '{}': No such file or directory", cp.target);
+        return;
+    }
+
+    let target = metadata.unwrap();
+
+    if target.is_file() {
+        if cp.options.len() != 1 {
+            eprintln!("cp: target '{}' is not a directory", cp.target);
             return;
         }
-
-        if cp.cible.is_empty() {
-            cp.cible = file;
-            continue;
-        }
-
-        cp.destinations.push(file);
-    }
-
-    let metadata = get_file_state(&cp.cible);
-    if metadata.is_err() {
-        eprintln!("cp: cannot stat '{}': No such file or directory", cp.cible);
-        return;
-    }
-
-    let data = metadata.unwrap();
-
-    if data.is_dir() && !cp.dir_flag {
-        eprintln!("cp: -r not specified; omitting directory '{}'", cp.cible);
-        return;
-    }
-
-    if data.is_file() {
-        match cp.read_file() {
+        match read_file(&cp.options[0]) {
             Ok(content) => {
                 if let Err(err) = cp.exec(content) {
                     eprintln!("cp: error copying file: {}", err);
                 }
             }
-            Err(err) => eprintln!("cp: error reading '{}': {}", cp.cible, err),
+            Err(err) => eprintln!("cp: error reading '{}': {}", cp.options[0], err),
         }
     }
 
-    if data.is_dir() && cp.dir_flag {
-        for dest in &cp.destinations {
-            let dest_path = Path::new(dest);
-            let src_path = Path::new(&cp.cible);
-
-            let new_dest_dir = if dest_path.is_dir() {
+    if target.is_dir() {
+        for opt in cp.options.iter() {
+            let src_path = Path::new(opt);
+            let dest_path = Path::new(&cp.target);
+            println!("{:?} =>{:?}", dest_path, src_path);
+            let new_src_dir = if src_path.is_dir() {
                 dest_path.join(src_path.file_name().unwrap())
             } else {
-                PathBuf::from(dest)
+                PathBuf::from(opt)
             };
 
-            if let Err(err) = Cp::copy_dir_recursive(src_path, &new_dest_dir) {
+            if let Err(err) = Cp::copy_dir_recursive(src_path, &new_src_dir) {
                 eprintln!("cp: cannot copy directory: {}", err);
                 return;
             }
@@ -157,4 +134,7 @@ pub fn direct_children(dir: &Path) -> Vec<PathBuf> {
         }
     }
     children
+}
+pub fn read_file(file: &String) -> io::Result<String> {
+    fs::read_to_string(file)
 }
