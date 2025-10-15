37a7d32fcb0f4ed25e51818ebdd4d2415efa4058
Mossab Idbenkellal <idmossab@gmail.com>
2025-10-15 08:57:25 +0100

    work about rm and finish remove file

--- src/commands/rm.rs
+++ src/commands/rm.rs
@@ -1,3 +1,28 @@
+use std::fs;
+use std::path::Path;
+
 pub fn rm_handler(args: Vec<String>) {
-    println!("{:?}", args);
+    if args.is_empty() {
+        eprintln!("Usage: rm <filename>");
+        return;
+    }
+
+    for filename in &args {
+        let path = Path::new(filename);
+        //println!("rrrrrrrrrrrrr {}",path.display());
+
+        if !path.exists() {
+            eprintln!("rm: cannot remove '{}': No such file or directory", filename);
+            continue;
+        }
+
+        if path.is_file() {
+            match fs::remove_file(path) {
+                Ok(_) => (),
+                Err(e) => eprintln!("rm: failed to remove '{}': {}", filename, e),
+            }
+        } else {
+            eprintln!("rm: cannot remove '{}': Is a directory", filename);
+        }
+    }
 }
