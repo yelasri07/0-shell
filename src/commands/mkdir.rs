e97d424fdead279b3fdb572c2def61958e9f0f7c
Ismail Sayen <ismailsvn02@gmail.com>
2025-10-16 16:55:43 +0100

    mkdir command done

--- src/commands/mkdir.rs
+++ src/commands/mkdir.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
 pub fn mkdir_handler(args: Vec<String>) {
- println!("{:?}", args);
+    if args.is_empty() {
+        eprintln!("mkdir: missing operand");
+    }
+    for path in args {
+        if let Err(err) = fs::create_dir(path) {
+            eprintln!("mkdir: {}", err);
+        }
+    }
 }
