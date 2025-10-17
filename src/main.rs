53115cc4d4969a4d97d0137e0f4c643d9ba69b95
yelasri <elasriyoussef604@gmail.com>
2025-10-17 12:24:08 +0100

    modify parsing

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,18 @@ fn main() {
     loop {
         shell.set_current_path(get_current_dir());
 
-        let (input,n_bytes) = read_line(&("~".to_owned() + shell.current_path.as_str() + "$"));
+        let (input, n_bytes) = read_line(&(shell.current_path.to_string() + "$"));
         if n_bytes == 0 {
             println!();
             break;
         }
-        shell.set_cmd("".to_string());
+
         shell.set_args(vec![]);
         shell.set_arg("".to_string());
-        shell.set_cmd_len(0);
 
-        if let Err(e) = shell.parse_cmd(input.as_str()) {
-            println!("{}", e);
-            continue;
-        }
+        shell.parse_input(input.as_str());
 
-        let cmd: &str = &shell.get_cmd();
-        if cmd == "exit" {
-            break;
-        }
-        let cmd_len = shell.get_cmd_len();
-
-        shell.parse_args(&input[cmd.len() + cmd_len..input.len()]);
+        // println!("{:?}", shell.args);
 
         shell.run();
     }
