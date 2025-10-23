9b6bc38f5c36ce5704550213d696a4130d8268fa
Mossab Idbenkellal <idmossab@gmail.com>
2025-10-23 15:06:20 +0100

    edit clear

--- src/commands/clear.rs
+++ src/commands/clear.rs
@@ -1,14 +1,6 @@
-pub fn clear_handler() {
-    // ANSI escape code to clear screen and move cursor to top-left
-    print!("\x1B[2J\x1B[1;1H");
+use std::io::{stdout, Write};
 
-    #[cfg(unix)]
-    {
-        print!("\x1B[3J\x1B[2J\x1B[1;1H");
-    }
-    
-    #[cfg(windows)]
-    {
-        print!("\x1B[2J\x1B[1;1H");
-    }
+pub fn clear_handler() {
+    print!("\x1B[2J\x1B[1;1H");
+    stdout().flush().unwrap();
 }
