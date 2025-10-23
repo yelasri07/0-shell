ac3ecb572ce5b370d0c1c3fe17538f3f12ed4729
Mossab Idbenkellal <idmossab@gmail.com>
2025-10-23 15:11:37 +0100

    finish clear

--- src/commands/clear.rs
+++ src/commands/clear.rs
@@ -1,6 +1,6 @@
-use std::io::{stdout, Write};
-
 pub fn clear_handler() {
+    // ANSI escape code to clear
     print!("\x1B[2J\x1B[1;1H");
-    stdout().flush().unwrap();
+    print!("\x1B[3J");
+
 }
