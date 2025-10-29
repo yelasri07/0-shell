53b695382c92dbef5f87f5055216a8350b471e6e
Mossab Idbenkellal <idmossab@gmail.com>
2025-10-29 09:41:05 +0100

    use display

--- src/commands/pwd.rs
+++ src/commands/pwd.rs
@@ -1,8 +1,5 @@
 use std::path::PathBuf;
 
 pub fn pwd_handler(path: &PathBuf) {
-      match path.to_str() {
-        Some(path_str) => println!("{}", path_str),
-        None => eprintln!("Error: Path invalid"),
-    }
+   println!("{}", path.display());
 }
