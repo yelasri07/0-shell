9fed1f7f7e8cda9de14392f8248b41403ddfb077
Mossab Idbenkellal <idmossab@gmail.com>
2025-10-30 10:15:21 +0100

    handler cat- with files before use flush

--- src/commands/cat.rs
+++ src/commands/cat.rs
@@ -2,24 +2,9 @@ use std::fs;
 use std::io::{self, BufRead, Read, Write};
 
 pub fn cat_handler(args: Vec<String>) {
-    // println!("DEBUG => args = {:?}", args);
-
     if args.is_empty() {
         // eprintln!("Usage: cat <filename>");
-        let stdin = io::stdin();
-        let mut stdout = io::stdout();
-
-        for line in stdin.lock().lines() {
-            match line {
-                Ok(content) => {
-                    writeln!(stdout, "{}", content).unwrap();
-                }
-                Err(e) => {
-                    eprintln!("cat: error reading from stdin: {}", e);
-                    break;
-                }
-            }
-        }
+        read_from_stdin();
         return;
     }
 
@@ -27,38 +12,41 @@ pub fn cat_handler(args: Vec<String>) {
         //println!("DEBUG: trying to open {}", filename);
         //cat -
         if filename == "-" {
-            let stdin = io::stdin();
-            let mut stdout = io::stdout();
-
-            for line in stdin.lock().lines() {
-                match line {
-                    Ok(content) => {
-                        writeln!(stdout, "{}", content).unwrap();
-                    }
-                    Err(e) => {
-                        eprintln!("cat: error reading from stdin: {}", e);
-                        break;
+            //Ensure that the output from previous files is printed before reading from stdin
+             io::stdout().flush().unwrap();
+             read_from_stdin();
+        }else{
+            match fs::File::open(filename) {
+                Ok(mut file) => {
+                    //println!("DEBUG: successfully opened {}", filename);
+                    let mut contents = String::new();
+                    if let Err(e) = file.read_to_string(&mut contents) {
+                        eprintln!("Failed to read file '{}': {}", filename, e);
+                    }else {
+                        //println!("DEBUG: read ok, printing content");
+                        print!("{}", contents);
                     }
                 }
+                Err(e) => eprintln!("Failed to open file '{}': {}", filename, e),
             }
-            continue; 
-        }
-
-        match fs::File::open(filename) {
-            Ok(mut file) => {
-                //println!("DEBUG: successfully opened {}", filename);
-
-                let mut contents = String::new();
-                if let Err(e) = file.read_to_string(&mut contents) {
-                    eprintln!("Failed to read file '{}': {}", filename, e);
-                } else {
-                    //println!("DEBUG: read ok, printing content");
-                    print!("{}", contents);
-                }
-            }
-
-            Err(e) => eprintln!("Failed to open file '{}': {}", filename, e),
         }
     }
     println!();
+}
+
+fn read_from_stdin() {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    for line in stdin.lock().lines() {
+        match line {
+            Ok(content) => {
+                writeln!(stdout, "{}", content).unwrap();
+            }
+            Err(e) => {
+                eprintln!("cat: error reading from stdin: {}", e);
+                break;
+            }
+        }
+    }
 }
